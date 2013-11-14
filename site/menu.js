4ff298888ebed7cf1a80da8e9061f2e995b0cb7d
JaceHeart <gestur.vagn@gmail.com>
2013-11-14 13:12:45 +0000

    Revert "menu auðveldir takkar"
    This reverts commit bb35aeee579b0d92f349cf634de6a4cd97a8a1a9.

--- site/menu.js
+++ site/menu.js
@@ -1,73 +1,64 @@
 
 
 
-var boxstartx = g_canvas.width/2-75;
-var boxstarty = g_canvas.height/2 - 150;
-var boxheight = 30;
-var boxwidth = 150;
-var buttonnr = 2;
-var buttonname = ["single player", "two player"];
-var clickbutton = [0, 0];
-
 
 function menu(ctx){
-
 
     util.clearCanvas(ctx);
     util.fillBox(ctx, 0, 0, g_canvas.width, g_canvas.height, "black");
 
-    buttons(ctx, buttonnr, buttonname);
+
+    util.fillBox(ctx,  g_canvas.width/2-75, 50, 150, 30, "orange");
+    var oldStyle = ctx.fillStyle;
+    ctx.fillStyle = "white";
+    ctx.font="bold 20px Arial";
+    ctx.fillText("Single Player", g_canvas.width/2-75, 50+20);
 
 
-    var buttony = boxstarty;
-
-    for(var i = 0; i < buttonnr; i++){
-        clickbutton[i] = buttonclick(buttony);
-        buttony += 50;
-
-    }
+    util.fillBox(ctx,  g_canvas.width/2-75, 100, 150, 30, "orange");
+    var oldStyle = ctx.fillStyle;
+    ctx.fillStyle = "white";
+    ctx.font="bold 20px Arial";
+    ctx.fillText("Two Player", g_canvas.width/2-75, 100+20);
 
 
+    var mousex = g_mouseX;
+    var mousey = g_mouseY;
 
-   if(eatKey(KEY_1) || (clickbutton[0] && g_button)){
+    var clickbutton1 = buttonclick(mousex, mousey, g_canvas.width/2-75, 50, 30, 150);
+
+    var clickbutton2 = buttonclick(mousex, mousey, g_canvas.width/2-75, 100, 30, 150);
+
+
+
+
+    if(eatKey(KEY_1) || (clickbutton1 && g_button)){
+        util.clearCanvas(ctx);
+        ctx.fillStyle = oldStyle;
         g_gamestyle = 1;
         fieldManager.init();
     }
 
-    if(eatKey(KEY_2) || (clickbutton[1] && g_button)){
+        if(eatKey(KEY_2) || (clickbutton2 && g_button)){
+        util.clearCanvas(ctx);
+        ctx.fillStyle = oldStyle;
         g_gamestyle = 2;
         fieldManager.init();
     }
-
 }
 
-function buttons(ctx, nr, name){
-
-    buttony = boxstarty;
+function buttons(nr){
 
     for(var i = 0; i < nr; i++){
-
         
-        util.fillBox(ctx,  boxstartx, buttony, 150, 30, "orange");
-        ctx.fillStyle = "white";
-        ctx.font = "bold 20px Arial";
-        ctx.fillText(name[i], boxstartx + 20, buttony+20);
-
-        buttony += 50;
+        util.fillBox(ctx,  g_canvas.width/2-75, 50, 150, 30, "orange");
 
     }
 
 }
 
 
-function buttonclick(boxstarty){
-    if(((g_mouseX >= boxstartx) && (g_mouseX <= boxstartx+boxwidth)) && 
-        ((g_mouseY >= boxstarty) && (g_mouseY <= boxstarty+boxheight))){
-            return 1;
-    }
-    else return 0;
-
-}unction buttonclick(mousex, mousey, boxstartx, boxstarty, boxheight, boxwidth){
+function buttonclick(mousex, mousey, boxstartx, boxstarty, boxheight, boxwidth){
     if(((mousex >= boxstartx) && (mousex <= boxstartx+boxwidth)) && 
         ((mousey >= boxstarty) && (mousey <= boxstarty+boxheight))){
             return true;
