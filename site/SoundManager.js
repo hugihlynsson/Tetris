e4523e5a18132a6a5d013f47d3be5fb885c6370c
sbleika <siljableika@gmail.com>
2013-11-17 22:26:38 +0000

    Background Sound but not with SM
    I have been trying all weekend to get the soundManager to work but my
    skils to programming is not good.. but i found a way to make background
    music sound ;) but it is not looping so i have to try to fix that..

--- site/SoundManager.js
+++ site/SoundManager.js
@@ -5,22 +5,21 @@
 // A field 'class'
 var SoundManager = function (){
     // Private variables and methods:
-    var _sounds = {};
-
+    //var _sounds = {};
+    var mainSound = new Audio('sounds/Tetris.mp3')
     // Public methods:
     return {
         addSound : function (name, file){
-            _sounds[name] = file;
+            _sounds[name] = new Audio(file) ;
             
             console.log('Added: ' + _sounds[name]);
         },
 
-        playSound : function (name){
-            _sounds[name]
+        playSound : function (){
+           this.mainSound.play();
         },
-        stopSound : function (name){
-
-        },
+        stopSound : function (name) {
+                    },
        
     };
 };
