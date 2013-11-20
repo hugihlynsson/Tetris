e6eb57500f4662786cdabc88c20db29676c2a9ef
Hugi Hlynsson <hugihlynsson@gmail.com>
2013-11-20 17:33:36 +0000

    Minor fix

--- site/Highscore.js
+++ site/Highscore.js
@@ -2,26 +2,46 @@
 // HIGHSCORE CLASS
 // ===============
 
-"use strict";
+'use strict';
 
 var Highscore = function (){
     // Private variables and methods:
-    var _sounds;
+
+    // Get scores from Localstorage if exist:
+    var _scores = [];
+    for (var i = 0; i < 5; i++) {
+		_scores[i] = window.localStorage.getItem('highscore' + i) || 0;
+    }
+
+    console.log(_scores);
+
+    // window.localStorage.getItem();
+    // window.localStorage.setItem();
+
     // Public methods:
     return {
-        add: function (name, file){
-            _sounds[name] = new Audio(file);
-            console.log('Added: ' + _sounds[name]);
-        },
+		addScore : function (newScore) {
+			var isHighscore = false;
 
-        play: function (name, doLoop){
-            var sound = _sounds[name];
-            sound.loop = doLoop;
-            sound.play();
-        },
-        stop: function (name) {
-            _sounds[name].play(false);
-        },
+			for (var i = 4; i >= 0; i--) {
+				if (newScore > _score[i]) {
+					isHighscore = true;
+				}
+			}
 
+			return isHighscore;
+		},
+		render   : function (ctx, cx, y) {
+			var boxWidth = 200;
+			ctx.fillStyle = 'white';
+	        ctx.font = '100 20px Helvetica';
+	        ctx.textAlign = 'center';
+			ctx.fillText('Highscores:', cx, y);
+			if (_scores)
+	        for (var i = 0; i < 5; i++) {
+				ctx.fillText(_scores[i], cx, y += 30);
+	        	if (_scores[i] == 0) break; // Should be == to compare to falsy
+	        }
+		}
     };
 };
---
+++ site/Score.js
@@ -0,0 +1,30 @@
+// ===========
+// SCORE CLASS
+// ===========
+
+// A field 'class'
+var Score = function (x, y, color, fontSize, font)
+{
+	// Private variables and methods:
+	var _x = x;
+	var _y = y;
+	var _color = color;
+	var _fontSize = fontSize;
+	var _font = font;
+	var _score = 0;
+
+	// Public methods:
+	return {
+		render : function (ctx) {
+			var oldStyle = ctx.fillStyle;
+			ctx.fillStyle = _color;
+			ctx.textAlign = 'left';
+			ctx.font = '100 ' + _fontSize + 'px ' + _font;
+			ctx.fillText('score: ' + _score, _x, _y+_fontSize);
+			ctx.fillStyle = oldStyle;
+		},
+		addScore : function (amount) { _score += amount; },
+		getScore : function () { return _score; },
+		setColor : function (color) { _color = color; },
+	};
+};
