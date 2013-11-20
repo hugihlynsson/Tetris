2cd8bc8cae09d92a50bb4a8af8ef4ba8e3d1b4d2
Hugi Hlynsson <hugihlynsson@gmail.com>
2013-11-20 21:14:34 +0000

    Fix bug in soundmanager and add reset score to Highscore

--- site/Highscore.js
+++ site/Highscore.js
@@ -16,24 +16,23 @@ var Highscore = function (){
     console.log(_scores);
 
     var _saveScores = function () {
-    	console.log(_scores);
-    	for (var i = 0; i < 5; i++) {
-    		window.localStorage.setItem('highscore' + i, _scores[i]);
-    	}
-    	console.log('Scores saved to localStorage');
-    }
+        console.log(_scores);
+        for (var i = 0; i < 5; i++) {
+            window.localStorage.setItem('highscore' + i, _scores[i]);
+        }
+        console.log('Scores saved to localStorage');
+    };
 
     // Public methods:
     return {
-		add : function (newScore) {
+		add: function (newScore) {
 			var isHighscore = true;
 			for (var i = 0; i < 5; i++) {
 				if (newScore > _scores[i]) {
 					isHighscore = true;
 					console.log(i);
-					for (var j = 4; j > i; j--) {
+                    for (var j = 4; j > i; j--) {
 						_scores[j] = _scores[j-1];
-						console.log('Copying from and to: ' + j-1 + ' ' + j)
 					}
 					_scores[i] = newScore;
 					break;
@@ -47,8 +46,7 @@ var Highscore = function (){
 
 			return isHighscore;
 		},
-		render   : function (ctx, cx, y) {
-			var boxWidth = 200;
+		render: function (ctx, cx, y) {
 			ctx.fillStyle = 'white';
 	        ctx.font = '100 20px Helvetica';
 	        ctx.textAlign = 'center';
@@ -56,8 +54,14 @@ var Highscore = function (){
 			if (_scores)
 	        for (var i = 0; i < 5; i++) {
 				ctx.fillText(_scores[i], cx, y += 30);
-	        	if (_scores[i] == 0) break; // Should be == to compare to falsy
+                if (_scores[i] == 0) break; // Should be == to compare to falsy
 	        }
-		}
+		},
+        reset: function () {
+            for (var i in _scores) {
+                _scores[i] = 0;
+            }
+            _saveScores();
+        }
     };
 };
--- site/SoundManager.js
+++ site/SoundManager.js
@@ -16,15 +16,28 @@ var SoundManager = function () {
         },
 
         play: function (name, doLoop) {
-            if ()
+            if (!_sounds[name]) {
+                console.log('Soundmanager: Tried to play sound ' + name + ' that does not exist!')
+                return;
+            }
             var sound = _sounds[name];
             sound.loop = doLoop;
             sound.play();
         },
+
         pause: function (name) {
+            if (!_sounds[name]) {
+                console.log('Soundmanager: Tried to pause sound ' + name + ' that does not exist!')
+                return;
+            }
             _sounds[name].pause();
-        }
+        },
+
         stop: function (name) {
+            if (!_sounds[name]) {
+                console.log('Soundmanager: Tried to stop sound ' + name + ' that does not exist!')
+                return;
+            }
             _sounds[name].pause();
             _sounds[name].currentTime = 0;
         },
