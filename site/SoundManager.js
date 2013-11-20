41f7f1b655c8d28741044748f0a366257cd26afc
Hugi Hlynsson <hugihlynsson@gmail.com>
2013-11-20 21:04:34 +0000

    Add pause and stop to soundManager

--- site/SoundManager.js
+++ site/SoundManager.js
@@ -4,24 +4,29 @@
 
 "use strict";
 
-var SoundManager = function (){
+var SoundManager = function () {
     // Private variables and methods:
     var _sounds = {};
 
     // Public methods:
     return {
-        add: function (name, file){
+        add: function (name, file) {
             _sounds[name] = new Audio(file);
             console.log('Added: ' + _sounds[name]);
         },
 
-        play: function (name, doLoop){
+        play: function (name, doLoop) {
+            if ()
             var sound = _sounds[name];
             sound.loop = doLoop;
             sound.play();
         },
+        pause: function (name) {
+            _sounds[name].pause();
+        }
         stop: function (name) {
-            _sounds[name].play(false);
+            _sounds[name].pause();
+            _sounds[name].currentTime = 0;
         },
 
     };
